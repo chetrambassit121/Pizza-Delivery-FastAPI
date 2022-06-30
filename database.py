0d45726dc042a2886b5b78ab9af38a88a8fbfc0f
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-30 17:57:56 -0400

    env and gitignore files

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from decouple import config
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine=create_engine('database_credentials', echo=True)
+engine=create_engine(config('database_credentials'), echo=True)
 
 Base=declarative_base()
 
