fdeec8aa1e6f16f888ba94a6a83b20e7076b2e7e
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-30 17:52:25 -0400

    setup database.py, created User in models.py

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine 
+from decouple import config
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine=create_engine('database_credentials', echo=True)
+
+Base=declarative_base()
+
+Session=sessionmaker()
