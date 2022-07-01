37bcabd4e45d2e017e8d1c6e871ceef996a80165
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-30 22:24:19 -0400

    refresh token function, tested route on Insomnia

--- auth_routes.py
+++ auth_routes.py
@@ -109,3 +109,26 @@ async def login(user:LoginModel, Authorize:AuthJWT=Depends()):
         detail="Invalid Username Or Password"
     )
 
+
+@auth_router.get('/refresh')
+async def refresh_token(Authorize:AuthJWT=Depends()):
+    """
+    ## Create a fresh token
+    This creates a fresh token. It requires an refresh token.
+    """
+
+
+    try:
+        Authorize.jwt_refresh_token_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Please provide a valid refresh token"
+        ) 
+
+    current_user=Authorize.get_jwt_subject()
+
+    
+    access_token=Authorize.create_access_token(subject=current_user)
+
+    return jsonable_encoder({"access":access_token})
