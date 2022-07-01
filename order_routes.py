3dc481d76ee001261762273a6ef26de72ff0ed93
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-30 22:51:35 -0400

    edited hello function

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,status
+from fastapi.exceptions import HTTPException
+from fastapi_jwt_auth import AuthJWT
+from models import User,Order
+from schemas import OrderModel,OrderStatusModel
+from database import Session , engine
+from fastapi.encoders import jsonable_encoder
+
 
 order_router=APIRouter(
     prefix="/orders",
@@ -7,5 +14,19 @@ order_router=APIRouter(
 
 
 @order_router.get('/')
-async def hello():
-    return {"message":"Hello World"} 
+async def hello(Authorize:AuthJWT=Depends()):
+
+    """
+        ## A sample hello world route
+        This returns Hello world
+    """
+
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+    return {"message":"Hello World"}
