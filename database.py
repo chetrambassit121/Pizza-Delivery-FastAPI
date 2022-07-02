2704804eec9de048720b05c89241c7d827b85a2a
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-02 17:09:08 -0400

    updated requirments

--- database.py
+++ database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine 
 from decouple import config
-import databases
+# import databases
 import sqlalchemy
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL= "postgresql://xwjrxsgmusggpm:cb0e9f72e5b8ce042440d58f25bc572a6dfb5c9b94b71ea1576171b69bf4e806@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d80sm6fqpidcjc"
 
-database = databases.Database(DATABASE_URL)
+# database = databases.Database(DATABASE_URL)
 
-metadata = sqlalchemy.MetaData()
+# metadata = sqlalchemy.MetaData()
 
 engine = sqlalchemy.create_engine(DATABASE_URL)
 
