82130d2cf593a306d9777a81de844b74cd7a1564
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-02 16:28:48 -0400

    created new engine

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 # engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
 # engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
-engine=create_engine("postgresql://postgres:xwjrxsgmusggpm:cb0e9f72e5b8ce042440d58f25bc572a6dfb5c9b94b71ea1576171b69bf4e806@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d80sm6fqpidcjc", echo=True)
+engine=create_engine("postgresql://xwjrxsgmusggpm:cb0e9f72e5b8ce042440d58f25bc572a6dfb5c9b94b71ea1576171b69bf4e806@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d80sm6fqpidcjc", echo=True)
 
 # database = databases.Database(DATABASE_URL)
 
