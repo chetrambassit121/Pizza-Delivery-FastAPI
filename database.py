4b58d41a82d22ccbc5d1600c1256351ef260f1c6
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-02 11:21:16 -0400

    heroku deployment

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from decouple import config
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
-engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
+engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
+# engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
 
 Base=declarative_base()
 
