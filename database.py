d2190eba91a6c4ba39ca95a2a528f1206bf606a0
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-02 16:45:03 -0400

    editing engine

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
 from sqlalchemy import create_engine 
 from decouple import config
-
+import databases
+import sqlalchemy
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+DATABASE_URL= "postgresql://xwjrxsgmusggpm:cb0e9f72e5b8ce042440d58f25bc572a6dfb5c9b94b71ea1576171b69bf4e806@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d80sm6fqpidcjc"
 
-# engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
-# engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
-engine=create_engine("postgresql://xwjrxsgmusggpm:cb0e9f72e5b8ce042440d58f25bc572a6dfb5c9b94b71ea1576171b69bf4e806@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d80sm6fqpidcjc", echo=True)
+database = databases.Database(DATABASE_URL)
 
-# database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
 
-# metadata = sqlalchemy.MetaData()
-
+engine = sqlalchemy.create_engine(DATABASE_URL)
 
 Base=declarative_base()
 
 Session=sessionmaker()
+
+
+
+# engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
+# engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
