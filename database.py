1ee5440f79bb84da672c4a3dc6fd97f422aa2563
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-02 16:19:28 -0400

    created new engine

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine 
 from decouple import config
+import sqlalchemy
 from sqlalchemy.orm import declarative_base, sessionmaker
+import databases
 
 # engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
-engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
+# engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
+engine=create_engine("postgresql://xwjrxsgmusggpm:cb0e9f72e5b8ce042440d58f25bc572a6dfb5c9b94b71ea1576171b69bf4e806@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d80sm6fqpidcjc")
+
+# database = databases.Database(DATABASE_URL)
+
+# metadata = sqlalchemy.MetaData()
+
 
 Base=declarative_base()
 
