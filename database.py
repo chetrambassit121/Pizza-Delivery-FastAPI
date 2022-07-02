309b05155b3056e71ee5502828cebbb7e677c3db
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-02 12:25:36 -0400

    fixing internal service 500 error in production

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from decouple import config
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
-engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
+# engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
 
 Base=declarative_base()
 
