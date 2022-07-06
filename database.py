9cbf06e704d11e6f40d102319d9ad2243ec99049
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-05 23:34:31 -0400

    edited database

--- database.py
+++ database.py
@@ -4,19 +4,20 @@ from decouple import config
 import sqlalchemy
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL= "postgresql://xwjrxsgmusggpm:cb0e9f72e5b8ce042440d58f25bc572a6dfb5c9b94b71ea1576171b69bf4e806@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d80sm6fqpidcjc"
+
 
 # database = databases.Database(DATABASE_URL)
 
 # metadata = sqlalchemy.MetaData()
 
-engine = sqlalchemy.create_engine(DATABASE_URL)
+# engine = sqlalchemy.create_engine(DATABASE_URL)
+engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
 
 Base=declarative_base()
+
+
 
 Session=sessionmaker()
 
 
 
-# engine=create_engine(config('DATABASE_CREDENTIALS'), echo=True)
-# engine=create_engine('postgresql://postgres:kitty121@localhost/Pizza-Delivery-FastAPI', echo=True)
